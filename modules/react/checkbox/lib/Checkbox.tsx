04caf5437c8060501be34c207da0b1447551431a
Thu Nguyen <thuminhnguyen@berkeley.edu>
2024-08-03 21:37:21 -0700

    fix(Checkbox): Remove console warning

--- modules/react/checkbox/lib/Checkbox.tsx
+++ modules/react/checkbox/lib/Checkbox.tsx
@@ -12,8 +12,8 @@ import {CheckboxInput, CheckboxProps} from './CheckboxInput';
 
 export const Checkbox = createComponent('input')({
   displayName: 'Checkbox',
-  Component: ({id, label = '', ...elemProps}: CheckboxProps, ref, Element) => {
-    const {checked = false, disabled, error, indeterminate, variant} = elemProps;
+  Component: ({id, indeterminate, label = '', ...elemProps}: CheckboxProps, ref, Element) => {
+    const {checked = false, disabled, error, variant} = elemProps;
     const inputId = useUniqueId(id);
     const {localRef, elementRef} = useLocalRef(ref);
 
